42bba1e1285394e53b436b6e13fbadeaa7aea8f5
Marina <marimos1986@gmail.com>
2019-12-01 23:01:03 +0300

    index final

--- script.js
+++ script.js
@@ -1,51 +1,58 @@
-ymaps.ready(function () {
-    var myMap = new ymaps.Map('map', {
-            center: [59.939068064173775,30.3273664999999],
-            zoom: 16
-        }, {
-            searchControlProvider: 'yandex#search'
-        }),
+(function () {
+    ymaps.ready(function () {
+        var myMap = new ymaps.Map('map', {
+                center: [59.939068064173775,30.3273664999999],
+                zoom: 16
+            }, {
+                searchControlProvider: 'yandex#search'
+            }),
 
-        // Создаём макет содержимого.
-        MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
-            '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
-        ),
+            // Создаём макет содержимого.
+            MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+                '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+            ),
 
-        myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
-            hintContent: 'Собственный значок метки',
-        }, {
-            // Опции.
-            // Необходимо указать данный тип макета.
-            iconLayout: 'default#image',
-            // Своё изображение иконки метки.
-            iconImageHref: './img/marker.png',
-            // Размеры метки.
-            iconImageSize: [218, 142],
-            // Смещение левого верхнего угла иконки относительно
-            // её "ножки" (точки привязки).
-            iconImageOffset: [-5, -38]
-        }),
+            myPlacemark = new ymaps.Placemark([59.938639906114595,30.32308247620295], {
+                hintContent: 'Собственный значок метки',
+            }, {
+                // Опции.
+                // Необходимо указать данный тип макета.
+                iconLayout: 'default#image',
+                // Своё изображение иконки метки.
+                iconImageHref: './img/marker.png',
+                // Размеры метки.
+                iconImageSize: [218, 142],
+                // Смещение левого верхнего угла иконки относительно
+                // её "ножки" (точки привязки).
+                iconImageOffset: [-40, -142]
+            });
 
-        myPlacemarkWithContent = new ymaps.Placemark([55.661574, 37.573856], {
-            hintContent: 'Собственный значок метки с контентом',
-            iconContent: '12'
-        }, {
-            // Опции.
-            // Необходимо указать данный тип макета.
-            iconLayout: 'default#imageWithContent',
-            // Своё изображение иконки метки..
-            iconImageSize: [48, 48],
-            // Смещение левого верхнего угла иконки относительно
-            // её "ножки" (точки привязки).
-            iconImageOffset: [-24, -24],
-            // Смещение слоя с содержимым относительно слоя с картинкой.
-            iconContentOffset: [15, 15],
-            // Макет содержимого.
-            iconContentLayout: MyIconContentLayout
-        });
+        myMap.geoObjects
+            .add(myPlacemark)
+    });
 
-    myMap.geoObjects
-        .add(myPlacemark)
-        .add(myPlacemarkWithContent);
-});
+    var link = document.querySelector(".link-feedback");
+    var popup = document.querySelector(".modal-feedback");
+    var close = document.querySelector(".modal-close");
+    var blackout = document.querySelector(".overlay");
+
+    link.addEventListener("click", function (evt) {
+        evt.preventDefault();
+        popup.classList.add("modal-show");
+        blackout.classList.add("modal-show");
+    });
+
+    close.addEventListener("click", function (evt) {
+        evt.preventDefault();
+        popup.classList.remove("modal-show");
+        blackout.classList.remove("modal-show");
+    });
+
+
+    blackout.addEventListener("click", function (evt) {
+        evt.preventDefault();
+        popup.classList.remove("modal-show");
+        blackout.classList.remove("modal-show");
+    });
+})();
 
